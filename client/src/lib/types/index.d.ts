e28b227543f8ad64b43e07bcc23f3991cf8cb47b
Frank van Zon <f.vanzon@outlook.com>
2025-02-13 17:09:52 +0100

    End of section 3

---
+++ client/src/lib/types/index.d.ts
@@ -0,0 +1,12 @@
+type Project = {
+    id: string
+    name: string
+    releaseDate: string
+    description: string
+    category: string
+    isCancelled: boolean
+    cluster: string
+    team: string
+    startQuarter: number
+    launchQuarter: number
+}
